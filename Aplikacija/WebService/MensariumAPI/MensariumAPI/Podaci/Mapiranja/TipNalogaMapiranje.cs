4809bb208debc4cdfb2e1ae2bf4f36462213c4a5
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2017-05-15 03:10:29 +0200

    sredjeno mapiranje - ispusten fajl

---
+++ Aplikacija/WebService/MensariumAPI/MensariumAPI/Podaci/Mapiranja/TipNalogaMapiranje.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using MensariumAPI.Podaci.Entiteti;
+
+namespace MensariumAPI.Podaci.Mapiranja
+{
+    public class TipNalogaMapiranje : ClassMap<TipNaloga>
+    {
+        public TipNalogaMapiranje()
+        {
+            Table("TipNaloga");
+
+            Id(x => x.IdTip, "idTip").GeneratedBy.Identity();
+
+            Map(x => x.Naziv, "naziv").Not.Nullable().Unique();
+
+            //TipNaloga <- Korisnici
+            HasMany(x => x.Korisnici).KeyColumn("tipNaloga")
+                .Cascade.SaveUpdate()
+                .Inverse();
+
+            //TipNaloga <-> Privilegija M:N
+            HasManyToMany(x => x.Privilegije)
+                .Table("PrivilegijeDodeljivanja")
+                .ParentKeyColumn("idTipNaloga")
+                .ChildKeyColumn("idPrivilegije")
+                .Cascade.SaveUpdate();
+
+        }
+    }
+}
