650f6d1be0f02f9be5e19328e185c802118801a5
stefan81888 <stefan.stankovic3@gmail.com>
2017-05-21 15:58:00 +0200

    Ispravljen korisnici kontroler

--- Aplikacija/WebService/MensariumAPI/MensariumAPI/Controllers/KorisniciController.cs
+++ Aplikacija/WebService/MensariumAPI/MensariumAPI/Controllers/KorisniciController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 using AutoMapper;
 using MensariumAPI.Podaci.Entiteti;
 using MensariumAPI.Podaci.Konfigracija;
@@ -12,201 +13,225 @@ using MensariumAPI.Podaci.ProvajderiPodataka;
 using MensariumAPI.Podaci.DTO;
 namespace MensariumAPI.Controllers
 {
-    [RoutePrefix("api/korisnici")]
-
-
+    [System.Web.Http.RoutePrefix("api/korisnici")]
     public class KorisniciController : ApiController
     {
-
-
-        //[HttpGet]
-        //public string Korisnik(int id)
-        //{
-            
-            
-        //    try
-        //    {
-        //        ISession ses = Podaci.ProvajderiPodataka.SesijeProvajder.VratiSesiju();
-        //        string odgovor;
-        //        Korisnik k = ses.Load<Korisnik>(id);
-                
-        //        odgovor = string.Format("ID_{0} {1} {2} {3} {4}",
-        //            k.IdKorisnika,
-        //            k.Ime,
-        //            k.Prezime,
-        //            k.KorisnickoIme,
-        //            k.Email
-        //        );
-        //        ses.Close();
-        //        return odgovor;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Konfiguracija.StampajIzuzetak(ex);
-        //    }
-        //    return null;
-        //}
-
-
-
-        [HttpGet]
-        [Route("full/{id:int}")]
-        public KorisnikFullDto VratiKorisnikaFull(int id)
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("full/{id:int}")]
+        public IHttpActionResult VratiKorisnikaFull(int id)
         {
-            SesijeProvajder.OtvoriSesiju();
-
-            Korisnik k = ProvajderPodataka.VratiKorisnika(id);
-            KorisnikFullDto korisnik = new KorisnikFullDto();
-            if (Validator.KorisnikPostoji(k))
+            try
             {
-                //TO-DO: PUCA ZA NULLABLE
-                //za sve nullable podatke ispitati da li su postavljeni
-                //i ako nije preskoci dodavanje u dto;
-                korisnik.KorisnickoIme = k.KorisnickoIme;
-                korisnik.Email = k.Email;
-                korisnik.Ime = k.Ime;
-                korisnik.Prezime = k.Prezime;
-                korisnik.DatumRodjenja = k.DatumRodjenja;
-                korisnik.DatumRegistracije = k.DatumRegistracije;
-                korisnik.BrojTelefona = k.BrojTelefona;
-                korisnik.BrojIndeksa = k.BrojIndeksa;
-                korisnik.DatumVaziDo = k.DatumVaziDo;
-                korisnik.AktivanNalog = k.AktivanNalog;
-                korisnik.IdTipaNaloga = k.TipNaloga.IdTip;
-                korisnik.IdFakulteta = k.StudiraFakultet.IdFakultet;
-                korisnik.IdObjave = k.Objava.IdObjave;
-            }
-            SesijeProvajder.ZatvoriSesiju();
-            return korisnik;
-        }
+                SesijeProvajder.OtvoriSesiju();
 
-        [HttpGet]
-        public List<KorisnikFullDto> VratiSveKorisnikeFull()
-        {
-            SesijeProvajder.OtvoriSesiju();
+                Korisnik k = ProvajderPodataka.VratiKorisnika(id);
 
-            IEnumerable<Korisnik> ienKorisnici = ProvajderPodataka.VratiKorisnike();
-            List<Korisnik> listaKorisnika = ienKorisnici.ToList();
-            List<KorisnikFullDto> listaKorisnikaFull = new List<KorisnikFullDto>(listaKorisnika.Count);
-
-            for (int i = 0; i < listaKorisnika.Count; ++i)
-            {
                 KorisnikFullDto korisnik = new KorisnikFullDto();
-                Korisnik k = listaKorisnika[i];
-
-                korisnik.KorisnickoIme = k.KorisnickoIme;
-                korisnik.Email = k.Email;
-                korisnik.Ime = k.Ime;
-                korisnik.Prezime = k.Prezime;
-                korisnik.DatumRodjenja = k.DatumRodjenja;
-                korisnik.DatumRegistracije = k.DatumRegistracije;
-                korisnik.BrojTelefona = k.BrojTelefona;
-                if (k.BrojIndeksa != null)
-                    korisnik.BrojIndeksa = k.BrojIndeksa;
-                if (k.DatumVaziDo != null)
-                    korisnik.DatumVaziDo = k.DatumVaziDo;
-                korisnik.AktivanNalog = k.AktivanNalog;
-                korisnik.IdTipaNaloga = k.TipNaloga.IdTip;
-                if (k.StudiraFakultet != null)
-                    korisnik.IdFakulteta = k.StudiraFakultet.IdFakultet;
-                if (k.Objava != null)
-                    korisnik.IdObjave = k.Objava.IdObjave;
-
-                
-
-                listaKorisnikaFull.Add(korisnik);
-            }
-            SesijeProvajder.ZatvoriSesiju();
-            return listaKorisnikaFull;
-        }
-
-        [HttpGet]
-        [Route("follow/{id:int}")]
-        public KorisnikFollowDto VratiKorisnikaFollow(int id)
-        {
-            SesijeProvajder.OtvoriSesiju();
-
-            Korisnik k = ProvajderPodataka.VratiKorisnika(id);
-            KorisnikFollowDto korisnik = new KorisnikFollowDto();
-            if (Validator.KorisnikPostoji(k))
-            {
-                //korisnik.IdKorisnika = k.IdKorisnika; ???
-                korisnik.KorisnickoIme = k.KorisnickoIme;
-                korisnik.Ime = k.Ime;
-                korisnik.Prezime = k.Prezime;
-                korisnik.Fakultet = k.StudiraFakultet.Naziv;
-                //korisnik.Zapracen ???
-            }
-
-            SesijeProvajder.ZatvoriSesiju();
-            return korisnik;
-        }
-
-        [HttpPost]
-        [Route("dodaj")]
-        public void DodajKorisnika([FromBody]KorisnikFullDto kdto)
-        {
-            SesijeProvajder.OtvoriSesiju();
-
-            Korisnik k = new Korisnik()
-            {
-                KorisnickoIme = kdto.KorisnickoIme,
-                Ime = kdto.Ime,
-                Prezime = kdto.Prezime,
-                DatumRegistracije = kdto.DatumRegistracije,
-                DatumRodjenja = kdto.DatumRodjenja,
-                DatumVaziDo = kdto.DatumVaziDo,
-                Email = kdto.Email,
-                BrojIndeksa = kdto.BrojIndeksa,
-                BrojTelefona = kdto.BrojTelefona
-            };
-
-            ProvajderPodataka.DodajKorisnika(k);
-
-            SesijeProvajder.ZatvoriSesiju();
-        }
-
-        [HttpPut]
-        [Route("update")]
-        public void UpdateKorisnika([FromBody]ClientZaRegistracijuDto klijentReg)
-        {
-            SesijeProvajder.OtvoriSesiju();
-
-            Korisnik k = ProvajderPodataka.VratiKorisnika(klijentReg.DodeljeniId);
-            if (Validator.KorisnikPostoji(k))
-            {
-                if (k.Sifra == klijentReg.DodeljenaLozinka)
+                if (Validator.KorisnikPostoji(k))
                 {
-                    if (Validator.PostojiUsername(klijentReg.NovaLozinka) == null)
-                    {
-                        k.KorisnickoIme = klijentReg.KorisnickoIme;
-                        k.Email = klijentReg.Email;
-                        k.Sifra = klijentReg.NovaLozinka;
-                        k.BrojTelefona = klijentReg.Telefon;
-                    }
-                    ProvajderPodataka.UpdateKorisnika(k);
+                    korisnik.KorisnickoIme = k.KorisnickoIme;
+                    korisnik.Email = k.Email;
+                    korisnik.Ime = k.Ime;
+                    korisnik.Prezime = k.Prezime;
+                    korisnik.DatumRodjenja = k.DatumRodjenja;
+                    korisnik.DatumRegistracije = k.DatumRegistracije;
+                    korisnik.BrojTelefona = k.BrojTelefona;
+                    if (k.BrojIndeksa != null)
+                        korisnik.BrojIndeksa = k.BrojIndeksa;
+                    if (k.DatumVaziDo != null)
+                        korisnik.DatumVaziDo = k.DatumVaziDo;
+                    korisnik.AktivanNalog = k.AktivanNalog;
+                    korisnik.IdTipaNaloga = k.TipNaloga.IdTip;
+                    if (k.StudiraFakultet != null)
+                        korisnik.IdFakulteta = k.StudiraFakultet.IdFakultet;
+                    if (k.Objava != null)
+                        korisnik.IdObjave = k.Objava.IdObjave;
+                    if (k.IdKorisnika != null)
+                        korisnik.IdKorisnika = k.IdKorisnika;
                 }
+                SesijeProvajder.ZatvoriSesiju();
+                if(korisnik != null)
+                    return Content(HttpStatusCode.Found, korisnik);
             }
-
-            SesijeProvajder.ZatvoriSesiju();
+            catch (Exception e)
+            {
+                
+            }
+            return Content(HttpStatusCode.BadRequest, "Korisnik nije pronadjen");
         }
 
-        [HttpGet]
-        [Route("prijava")]
-        public SesijaDto Prijava([FromBody]ClientLoginDto cdto)
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult VratiSveKorisnikeFull()
         {
-            SesijeProvajder.OtvoriSesiju();
-
-            Korisnik k = new Korisnik()
+            try
             {
-                KorisnickoIme = cdto.KorisnickoIme,
-                Email = cdto.Email,
-                Sifra = cdto.Sifra
-            };
+                SesijeProvajder.OtvoriSesiju();
 
-            SesijeProvajder.ZatvoriSesiju();
+                IEnumerable<Korisnik> ienKorisnici = ProvajderPodataka.VratiKorisnike();
+                List<Korisnik> listaKorisnika = ienKorisnici.ToList();
+                List<KorisnikFullDto> listaKorisnikaFull = new List<KorisnikFullDto>(listaKorisnika.Count);
 
-            return ProvajderPodataka.PrijavaKorisnika(k);
+                for (int i = 0; i < listaKorisnika.Count; ++i)
+                {
+                    KorisnikFullDto korisnik = new KorisnikFullDto();
+                    Korisnik k = listaKorisnika[i];
+
+                    korisnik.KorisnickoIme = k.KorisnickoIme;
+                    korisnik.Email = k.Email;
+                    korisnik.Ime = k.Ime;
+                    korisnik.Prezime = k.Prezime;
+                    korisnik.DatumRodjenja = k.DatumRodjenja;
+                    korisnik.DatumRegistracije = k.DatumRegistracije;
+                    korisnik.BrojTelefona = k.BrojTelefona;
+                    if (k.BrojIndeksa != null)
+                        korisnik.BrojIndeksa = k.BrojIndeksa;
+                    if (k.DatumVaziDo != null)
+                        korisnik.DatumVaziDo = k.DatumVaziDo;
+                    korisnik.AktivanNalog = k.AktivanNalog;
+                    korisnik.IdTipaNaloga = k.TipNaloga.IdTip;
+                    if (k.StudiraFakultet != null)
+                        korisnik.IdFakulteta = k.StudiraFakultet.IdFakultet;
+                    if (k.Objava != null)
+                        korisnik.IdObjave = k.Objava.IdObjave;
+                    if (k.IdKorisnika != null)
+                        korisnik.IdKorisnika = k.IdKorisnika;
+
+
+                    listaKorisnikaFull.Add(korisnik);
+                }
+                SesijeProvajder.ZatvoriSesiju();
+                if (listaKorisnikaFull != null)
+                    return Content(HttpStatusCode.Found, listaKorisnikaFull);
+            }
+            catch (Exception e)
+            {
+            }
+            return Content(HttpStatusCode.BadRequest, "Korisnici nije pronadjen");
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("zaprati/{pratilac:int}/{praceni:int}")]
+        public IHttpActionResult Zaprati(int pratilac, int praceni)
+        {
+            try
+            {
+                SesijeProvajder.OtvoriSesiju();
+
+                Korisnik k = ProvajderPodataka.VratiKorisnika(pratilac);
+                KorisnikFollowDto korisnik = new KorisnikFollowDto();
+                if (Validator.KorisnikPostoji(k))
+                {
+                    //korisnik.IdKorisnika = k.IdKorisnika; ???
+                    korisnik.KorisnickoIme = k.KorisnickoIme;
+                    korisnik.Ime = k.Ime;
+                    korisnik.Prezime = k.Prezime;
+                    korisnik.Fakultet = k.StudiraFakultet.Naziv;
+                    //korisnik.Zapracen ???
+                }
+
+                SesijeProvajder.ZatvoriSesiju();
+                if(korisnik != null)
+                    return Content(HttpStatusCode.Found, korisnik);
+            }
+            catch (Exception e)
+            {
+            }
+            return Content(HttpStatusCode.BadRequest, "Akcija neuspešna");
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("dodaj")]
+        public IHttpActionResult DodajKorisnika([FromBody]KorisnikFullDto kdto)
+        {
+            try
+            {
+                SesijeProvajder.OtvoriSesiju();
+
+                Korisnik k = new Korisnik()
+                {
+                    KorisnickoIme = kdto.KorisnickoIme,
+                    Ime = kdto.Ime,
+                    Prezime = kdto.Prezime,
+                    DatumRegistracije = kdto.DatumRegistracije,
+                    DatumRodjenja = kdto.DatumRodjenja,
+                    DatumVaziDo = kdto.DatumVaziDo,
+                    Email = kdto.Email,
+                    BrojIndeksa = kdto.BrojIndeksa,
+                    BrojTelefona = kdto.BrojTelefona
+                };
+
+                ProvajderPodataka.DodajKorisnika(k);
+                SesijeProvajder.ZatvoriSesiju();
+
+                return Content(HttpStatusCode.OK, "");
+            }
+            catch (Exception e)
+            {
+            }
+            return Content(HttpStatusCode.BadRequest, "");
+
+        }
+
+        [System.Web.Http.HttpPut]
+        [System.Web.Http.Route("update")]
+        public IHttpActionResult UpdateKorisnika([FromBody]ClientZaRegistracijuDto klijentReg)
+        {
+            try
+            {
+                SesijeProvajder.OtvoriSesiju();
+
+                Korisnik k = ProvajderPodataka.VratiKorisnika(klijentReg.DodeljeniId);
+                if (Validator.KorisnikPostoji(k))
+                {
+                    if (k.Sifra == klijentReg.DodeljenaLozinka)
+                    {
+                        if (Validator.PostojiUsername(klijentReg.NovaLozinka) == null)
+                        {
+                            k.KorisnickoIme = klijentReg.KorisnickoIme;
+                            k.Email = klijentReg.Email;
+                            k.Sifra = klijentReg.NovaLozinka;
+                            k.BrojTelefona = klijentReg.Telefon;
+                        }
+                        ProvajderPodataka.UpdateKorisnika(k);
+                    }
+                }
+
+                SesijeProvajder.ZatvoriSesiju();
+                return Content(HttpStatusCode.OK, "");
+            }
+            catch (Exception e)
+            {
+            }
+            return Content(HttpStatusCode.BadRequest, "");
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("prijava")]
+        public IHttpActionResult Prijava([FromBody]ClientLoginDto cdto)
+        {
+            try
+            {
+                SesijeProvajder.OtvoriSesiju();
+
+                Korisnik k = new Korisnik()
+                {
+                    KorisnickoIme = cdto.KorisnickoIme,
+                    Email = cdto.Email,
+                    Sifra = cdto.Sifra
+                };
+
+                SesijeProvajder.ZatvoriSesiju();
+
+                SesijaDto sdto = ProvajderPodataka.PrijavaKorisnika(k);
+                if(sdto != null)
+                    return Content(HttpStatusCode.Found, sdto);
+            }
+            catch (Exception e)
+            {
+
+            }
+            return Content(HttpStatusCode.BadRequest, "");
+
         }
 
 
--- Aplikacija/WebService/MensariumAPI/MensariumAPI/Podaci/DTO/KorisnikFollowDto.cs
+++ Aplikacija/WebService/MensariumAPI/MensariumAPI/Podaci/DTO/KorisnikFollowDto.cs
@@ -7,7 +7,7 @@ namespace MensariumAPI.Podaci.DTO
 {
     public class KorisnikFollowDto
     {
-        public int IdKorisnika { get; protected set; }
+        public int IdKorisnika { get; set; }
         public string KorisnickoIme { get; set; }
         public string Ime { get; set; }
         public string Prezime { get; set; }
