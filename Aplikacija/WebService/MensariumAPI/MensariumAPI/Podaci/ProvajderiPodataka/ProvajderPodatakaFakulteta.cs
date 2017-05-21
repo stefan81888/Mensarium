2fe8f396fa097ae7f112c1c8609cc787f22bf0e7
Marija Stojković <stojkovicmarija95@gmail.com>
2017-05-21 15:52:40 +0200

    FakultetiController done.

---
+++ Aplikacija/WebService/MensariumAPI/MensariumAPI/Podaci/ProvajderiPodataka/ProvajderPodatakaFakulteta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MensariumAPI.Podaci.DTO;
+using MensariumAPI.Podaci.Entiteti;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace MensariumAPI.Podaci.ProvajderiPodataka
+{
+    public class ProvajderPodatakaFakulteta
+    {
+        public static Fakultet VratiFakultet(int id)
+        {
+            ISession s = SesijeProvajder.Sesija;
+            Fakultet f = s.Load<Fakultet>(id);
+            return f;
+        }
+
+        public static IEnumerable<Fakultet> VratiFakultete()
+        {
+            ISession s = SesijeProvajder.Sesija;
+            IEnumerable<Fakultet> fakulteti = s.Query<Fakultet>().Select(k => k);
+            return fakulteti;
+        }
+
+        public static void DodajFakultet(Fakultet f)
+        {
+            ISession s = SesijeProvajder.Sesija;
+            s.Save(f);
+            s.Flush();
+            s.Close();
+        }
+    }
+}
