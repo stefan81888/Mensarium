a31bfddb0a81720064ef7ca126739ad88541f694
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2017-05-21 16:47:23 +0200

    sredivanje settings klase

---
+++ Aplikacija/Desktop/MensariumDesktop/MensariumDesktop/Model/Components/MSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MensariumDesktop.Model.Components
+{
+    public static class MSettings
+    {
+        public static Session CurrentSession { get; set; }
+        public static Server Server { get; set; }
+
+        static MSettings()
+        {
+            Server = new Server()
+            {
+                serverIP = "localhost",
+                serverPort = "2244"
+            };
+        }
+
+        public static void LoadSettings() { }
+        public static void SaveSettings() { }
+    }
+}
