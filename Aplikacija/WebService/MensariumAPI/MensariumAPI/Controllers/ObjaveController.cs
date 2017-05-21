cf671361bce5f00de53deb1768ace4e6c0fcef70
stefan81888 <stefan.stankovic3@gmail.com>
2017-05-22 00:25:38 +0200

    Objave početak

--- Aplikacija/WebService/MensariumAPI/MensariumAPI/Controllers/ObjaveController.cs
+++ Aplikacija/WebService/MensariumAPI/MensariumAPI/Controllers/ObjaveController.cs
@@ -5,15 +5,51 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using MensariumAPI.Podaci.DTO;
+using MensariumAPI.Podaci.ProvajderiPodataka;
 
 namespace MensariumAPI.Controllers
 {
+    [System.Web.Http.RoutePrefix("api/objave")]
     public class ObjaveController : ApiController
     {
         [HttpGet]
-        public ObjavaFullDto PrikaziSveObjave()
+        [System.Web.Http.Route("prikazi/{id:int}")]
+        public IHttpActionResult PrikaziSveObjave(int id)
         {
-            return null;
+            try
+            {
+                SesijeProvajder.OtvoriSesiju();
+
+                ObjavaFullDto o = ProvajderPodatakaObjava.VratiObjavu(id);
+
+                SesijeProvajder.ZatvoriSesiju();
+
+                return Content(HttpStatusCode.Found, o);
+            }
+            catch (Exception e)
+            {
+            }
+            return Content(HttpStatusCode.BadRequest, new ObjavaFullDto());
+        }
+
+        [HttpPost]
+        [System.Web.Http.Route("objavi/{id:int}")]
+        public IHttpActionResult Objavi(int id, [FromBody] ObjavaCUDto ocdto)
+        {
+            try
+            {
+                SesijeProvajder.OtvoriSesiju();
+
+                //ObjavaFullDto o = ProvajderPodatakaObjava.VratiObjavu(id);
+
+                SesijeProvajder.ZatvoriSesiju();
+
+                return Content(HttpStatusCode.Found, "");
+            }
+            catch (Exception e)
+            {
+            }
+            return Content(HttpStatusCode.BadRequest, new ObjavaFullDto());
         }
     }
 }
