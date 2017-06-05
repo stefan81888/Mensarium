ae2442b0c23f28c3e1b701593196009c5e85856c
Nikola Savic <nikolaaasavic@live.com>
2017-06-05 02:22:49 +0200

    Citanje novosti sa servera

--- Aplikacija/Android/Mensarium/Mensarium/Resources/fragments/NewsFragment.cs
+++ Aplikacija/Android/Mensarium/Mensarium/Resources/fragments/NewsFragment.cs
@@ -7,25 +7,45 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V4.Widget;
 using Android.Views;
 using Android.Widget;
+using Mensarium.Components;
+using MensariumDesktop.Model.Components.DTOs;
 
 namespace Mensarium
 {
     class NewsFragment : Android.Support.V4.App.Fragment
     {
+        private SwipeRefreshLayout swipe;
+        private List<ObjavaReadDto> lista;
+        private ListView listView;
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var view = inflater.Inflate(Resource.Layout.NewsFragment, container, false);
 
-            List<FeedItem> lista = new List<FeedItem>();
-            lista.Add(new FeedItem() { feedIme = "Nikola Savic", feedVreme = "18h", status = "Pomfrit sada. Brzo!"});
-            lista.Add(new FeedItem() { feedIme = "Petar Peric", feedVreme = "14h", status = "Pasulj njah!"});
+            lista = new List<ObjavaReadDto>();
+            //lista.Add(new FeedItem() { feedIme = "Nikola Savic", feedVreme = "18h", status = "Pomfrit sada. Brzo!"});
+            //lista.Add(new FeedItem() { feedIme = "Petar Peric", feedVreme = "14h", status = "Pasulj njah!"});
 
-            var listView = view.FindViewById<ListView>(Resource.Id.FeedListView);
+            lista = Api.Api.GetFollowedUsersPosts(MSettings.CurrentSession.LoggedUser.UserID);
+
+            listView = view.FindViewById<ListView>(Resource.Id.FeedListView);
             listView.Adapter = new FeedListAdapter(this, lista);
 
+            swipe = view.FindViewById<SwipeRefreshLayout>(Resource.Id.swipeContainer);
+            swipe.Refresh += Swipe_Refresh;
             return view;
+        }
+
+        private void Swipe_Refresh(object sender, EventArgs e)
+        {
+            lista = Api.Api.GetFollowedUsersPosts(MSettings.CurrentSession.LoggedUser.UserID);
+
+            listView.Adapter = new FeedListAdapter(this, lista);
+
+            swipe.Refreshing = false;
         }
     }
 }
